4fd98295475540f3f44ebae545c4c087cc5c349e
mimi-2023 <karasu@LAPTOP-L90LNOLE>
2024-08-13 17:38:39 +0900

    作業途中 #21

--- apis/routers/slack.py
+++ apis/routers/slack.py
@@ -1,6 +1,7 @@
 import os
 from fastapi import APIRouter, Request
 from slack_bolt.async_app import AsyncApp
+from slack_bolt.oauth.oauth_settings import OAuthSettings
 from slack_bolt.adapter.fastapi.async_handler import AsyncSlackRequestHandler
 import logging
 
@@ -10,14 +11,36 @@ logger = logging.getLogger(__name__)
 
 router = APIRouter(tags=["Slack"])
 
-# Slackアプリの設定
-slack_app = AsyncApp(
-    token=os.environ.get("SLACK_BOT_TOKEN"),
-    signing_secret=os.environ.get("SLACK_SIGNING_SECRET"),
-    # process_before_response=True,
-    # request_verification_enabled=False,
-    # ignoring_self_events_enabled=False  # self eventsの無視を無効にする
+# OAuth設定
+oauth_settings = OAuthSettings(
+    client_id=os.environ.get("SLACK_CLIENT_ID"),
+    client_secret=os.environ.get("SLACK_CLIENT_SECRET"),
+
+    scopes=["channels:read", "channels:manage", "groups:read", "users:read", "chat:write"],
+    installation_store={ 
+        "store": lambda installation: store_installation(installation),
+        "fetch": lambda query: fetch_installation(query),
+        # "store_org_installation": lambda installation: store_org_installation(installation),
+        # "fetch_org_installation": lambda query: fetch_org_installation(query),
+    },
 )
+
+# Bolt for PythonのApp初期化
+slack_app = AsyncApp(
+    signing_secret=os.environ.get("SLACK_SIGNING_SECRET"),
+    oauth_settings=oauth_settings
+)
+
+
+# # Slackアプリの設定
+# slack_app = AsyncApp(
+#     token=os.environ.get("SLACK_BOT_TOKEN"),
+#     signing_secret=os.environ.get("SLACK_SIGNING_SECRET"),
+#     # process_before_response=True,
+#     # request_verification_enabled=False,
+#     # ignoring_self_events_enabled=False  # self eventsの無視を無効にする
+# )
+
 handler = AsyncSlackRequestHandler(slack_app)
 
 # slack_appを定義した後でslack_eventsをインポートする
